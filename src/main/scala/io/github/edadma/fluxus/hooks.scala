1723f796c3f8d8a9408e3bcc7589347b74f6da69
Edward A Maxedon <edadma@gmail.com>
2024-11-24 00:30:22 -0500

    rewrite useState()

--- src/main/scala/io/github/edadma/fluxus/hooks.scala
+++ src/main/scala/io/github/edadma/fluxus/hooks.scala
@@ -14,32 +14,31 @@ package io.github.edadma.fluxus // Define the package namespace for the Fluxus f
 // The `useState` hook allows components to have state
 case class Update[T](func: T => T)
 
+case class StateHook[T](var state: T, setState: (T | Update[T]) => Unit)
+
 def useState[T](initialValue: T): (T, (T | Update[T]) => Unit) = {
-  val instance = RenderContext.currentInstance // Get the current component instance from RenderContext
+  val instance         = RenderContext.currentInstance
+  val currentHookIndex = instance.hookIndex
 
-  val currentHookIndex = instance.hookIndex // Capture the current hook index for this hook
-
-  // If this is the first render or the hook hasn't been initialized, initialize the state
   if (instance.hooks.size <= currentHookIndex) {
-    instance.hooks += initialValue // Add the initial value to the hooks array
-  }
-
-  val state = instance.hooks(currentHookIndex).asInstanceOf[T] // Retrieve the current state value from the hooks array
-
-  instance.hookIndex += 1 // Increment the hook index for the next hook
-
-  val setState: ((T | Update[T]) => Unit) = (newValueOrFunc: (T | Update[T])) => {
-    val newValue = newValueOrFunc match {
-      case Update(func: (T => T) @unchecked) =>
-        val f = func.asInstanceOf[T => T]
-        f(state)
-      case value: T @unchecked => value
+    var stateHook: StateHook[T] = null
+    val setState: ((T | Update[T]) => Unit) = (newValueOrFunc: (T | Update[T])) => {
+      val newValue = newValueOrFunc match {
+        case Update(func: (T => T) @unchecked) =>
+          func(stateHook.state)
+        case value: T @unchecked => value
+      }
+      stateHook.state = newValue
+      renderApp()
     }
-    instance.hooks(currentHookIndex) = newValue // Update the state in the hooks array with the new value
-    renderApp()                                 // Re-render the app starting from the root component
+    stateHook = StateHook(initialValue, setState)
+    instance.hooks += stateHook
   }
 
-  (state, setState) // Return the current state and the setState function as a tuple
+  val stateHook = instance.hooks(currentHookIndex).asInstanceOf[StateHook[T]]
+  instance.hookIndex += 1
+
+  (stateHook.state, stateHook.setState)
 }
 
 // The `useEffect` hook allows side-effects in functional components.
