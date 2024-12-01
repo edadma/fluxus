8c4e4b0e43f0080acfb160bdfd7ddc7d5aa95868
Edward A Maxedon <edadma@gmail.com>
2024-12-01 09:16:50 -0500

    logger

--- src/main/scala/io/github/edadma/fluxus/KeyedProps.scala
+++
@@ -1,4 +0,0 @@
-package io.github.edadma.fluxus
-
-trait KeyedProps:
-  def key: String
--- src/main/scala/io/github/edadma/fluxus/package.scala
+++
@@ -1,9 +0,0 @@
-package io.github.edadma.fluxus
-
-type FluxusComponent = Product => (FluxusNode | Null)
-
-type FC[Props <: Product] = Props => (FluxusNode | Null)
-
-case class EmptyProps()
-
-val emptyProps = EmptyProps()
