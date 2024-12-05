f9d3026cc9c85d8c3657d3c151a09d321cd13a00
Edward A Maxedon <edadma@gmail.com>
2024-12-05 17:57:12 -0500

    dom tests

--- src/main/scala/io/github/edadma/fluxus/examples/CounterApp.scala
+++ src/main/scala/io/github/edadma/fluxus/examples/CounterApp.scala
@@ -52,12 +52,12 @@ object CounterApp {
       div(
         cls := "flex gap-4",
         button(
-          cls     := "px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600",
+          cls     := "btn btn-primary",
           onClick := handleIncrement,
           "+",
         ),
         button(
-          cls     := "px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600",
+          cls     := "btn btn-secondary",
           onClick := handleDecrement,
           "-",
         ),
