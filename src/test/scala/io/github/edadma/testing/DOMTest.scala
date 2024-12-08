6bcaf2abbaaf94e501c11bb1a1758da3767c3102
Edward A Maxedon <edadma@gmail.com>
2024-12-08 17:21:04 -0500

    first unit tests

--- src/test/scala/io/github/edadma/testing/DOMTest.scala
+++ src/test/scala/io/github/edadma/testing/DOMTest.scala
@@ -1,7 +1,7 @@
-package io.github.edadma.fluxus.core
+package io.github.edadma.fluxus.testing
 
 import io.github.edadma.fluxus.TextNode
-import io.github.edadma.testing.DOMSpec
+import io.github.edadma.core.createDOMNode
 import org.scalajs.dom
 import org.scalajs.dom.Node
 
