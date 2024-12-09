6d496c160cbe348b47257ae123407f1bc287f0af
Edward A Maxedon <edadma@gmail.com>
2024-12-09 17:31:17 -0500

    createDOM unit tests

--- src/main/scala/io/github/edadma/fluxus/core/createDOM.scala
+++ src/main/scala/io/github/edadma/fluxus/core/createDOM.scala
@@ -5,15 +5,37 @@ import org.scalajs.dom.{Element, Node, document}
 
 def createDOMNode(node: FluxusNode): Node = {
   val dom = node match
-    case TextNode(text, _, _) =>
-      document.createTextNode(text)
-    case ElementNode(tag, props, events, _, _, _, _, namespace, _) =>
+    case TextNode(text, _, _)                                      => document.createTextNode(text)
+    case ElementNode(tag, attrs, events, _, _, _, _, namespace, _) =>
       // Create element with proper namespace if specified
-      namespace match
-        case Some(ns) => document.createElementNS(ns, tag)
-        case None     => document.createElement(tag)
-  //    case _: ComponentNode                            =>
+      val elem =
+        namespace match
+          case Some(ns) => document.createElementNS(ns, tag)
+          case None     => document.createElement(tag)
+      //    case _: ComponentNode                            =>
 
+      // Set attributes based on their type
+      attrs.foreach { case (name, value) =>
+        value match
+          case b: Boolean =>
+            // Only set attribute if true, remove if false
+            if (b) elem.setAttribute(name, "")
+            else elem.removeAttribute(name)
+
+          case null | None =>
+            // Remove attribute for null/None values
+            elem.removeAttribute(name)
+
+          case Some(v) =>
+            // Handle Option values
+            elem.setAttribute(name, v.toString)
+
+          case v =>
+            // Handle all other values by converting to string
+            elem.setAttribute(name, v.toString)
+      }
+
+      elem
   // Store the created DOM node
   node.domNode = Some(dom.asInstanceOf[Node])
   dom.asInstanceOf[Node]
--- src/test/scala/io/github/edadma/fluxus/testing/DOMTest.scala
+++ src/test/scala/io/github/edadma/fluxus/testing/DOMTest.scala
@@ -42,4 +42,66 @@ class DOMTest extends DOMSpec {
     container.textContent shouldBe "Hello world"
     container.firstChild shouldBe node.domNode.get
   }
+
+  "createDOMNode" should "set string attributes on elements" in {
+    val node = ElementNode(
+      tag = "div",
+      attrs = Map(
+        "class"     -> "test-class",
+        "id"        -> "test-id",
+        "data-test" -> "test-value",
+      ),
+      events = Map(),
+      children = Vector(),
+      parent = None,
+      domNode = None,
+      key = None,
+    )
+
+    val domNode = createDOMNode(node).asInstanceOf[dom.Element]
+
+    domNode.getAttribute("class") shouldBe "test-class"
+    domNode.getAttribute("id") shouldBe "test-id"
+    domNode.getAttribute("data-test") shouldBe "test-value"
+  }
+
+  it should "handle boolean attributes correctly" in {
+    val node = ElementNode(
+      tag = "input",
+      attrs = Map(
+        "disabled" -> true,
+        "checked"  -> false,
+      ),
+      events = Map(),
+      children = Vector(),
+      parent = None,
+      domNode = None,
+      key = None,
+    )
+
+    val domNode = createDOMNode(node).asInstanceOf[dom.Element]
+
+    domNode.hasAttribute("disabled") shouldBe true
+    domNode.hasAttribute("checked") shouldBe false
+  }
+
+  it should "handle numeric attributes" in {
+    val node = ElementNode(
+      tag = "input",
+      attrs = Map(
+        "maxLength" -> 50,
+        "tabIndex"  -> 1,
+      ),
+      events = Map(),
+      children = Vector(),
+      parent = None,
+      domNode = None,
+      key = None,
+    )
+
+    val domNode = createDOMNode(node).asInstanceOf[dom.Element]
+
+    domNode.getAttribute("maxLength") shouldBe "50"
+    domNode.getAttribute("tabIndex") shouldBe "1"
+  }
 }
