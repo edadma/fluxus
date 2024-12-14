3ab8a67232e364ee41fe6617d3e0f0eb9079b693
Edward A Maxedon <edadma@gmail.com>
2024-12-13 23:54:42 -0500

    reconciler

--- src/main/scala/io/github/edadma/fluxus/core/diff.scala
+++ src/main/scala/io/github/edadma/fluxus/core/diff.scala
@@ -78,16 +78,106 @@ private def diffSameType(oldNode: FluxusNode, newNode: FluxusNode): Seq[DOMOpera
   }
 }
 
+//private def diffProps(oldNode: ElementNode, newNode: ElementNode): Seq[DOMOperation] = {
+//  val propsToRemove = oldNode.attrs.keySet -- newNode.attrs.keySet
+//  val propsToAdd = newNode.attrs.filter { case (k, v) =>
+//    !oldNode.attrs.get(k).contains(v)
+//  }
+//
+//  // 1. Events that exist in old but not in new should be removed
+//  val removedEvents = oldNode.events.keySet -- newNode.events.keySet
+//
+//  // 2. New events that didn't exist before should be added
+//  val addedEvents = newNode.events.keySet -- oldNode.events.keySet
+//
+//  // 3. For events that exist in both, we need to update if they've changed
+//  val commonEvents = oldNode.events.keySet.intersect(newNode.events.keySet)
+//
+//  // Track which common events need updating by comparing the function references
+//  val eventsToUpdate = commonEvents.filter { eventName =>
+//    // If the function references are different, we need to update
+//    oldNode.events(eventName) ne newNode.events(eventName)
+//  }
+//
+//  val eventsToRemove = removedEvents ++ eventsToUpdate
+//  val eventsToAdd = newNode.events.filter { case (name, _) =>
+//    addedEvents.contains(name) || eventsToUpdate.contains(name)
+//  }
+//
+//  if (
+//    propsToRemove.nonEmpty || propsToAdd.nonEmpty ||
+//    eventsToRemove.nonEmpty || eventsToAdd.nonEmpty
+//  )
+//    Seq(UpdateProps(oldNode, propsToRemove, propsToAdd, eventsToRemove, eventsToAdd))
+//  else
+//    Nil
+//}
+
 private def diffProps(oldNode: ElementNode, newNode: ElementNode): Seq[DOMOperation] = {
+  logger.debug(
+    "Starting props diff",
+    category = "Reconciler",
+    opId = 1,
+    Map(
+      "oldEvents" -> oldNode.events.keySet.toString,
+      "newEvents" -> newNode.events.keySet.toString,
+    ),
+  )
+
   val propsToRemove = oldNode.attrs.keySet -- newNode.attrs.keySet
   val propsToAdd = newNode.attrs.filter { case (k, v) =>
     !oldNode.attrs.get(k).contains(v)
   }
 
-  val eventsToRemove = oldNode.events.keySet -- newNode.events.keySet
-  val eventsToAdd = newNode.events.filter { case (k, v) =>
-    !oldNode.events.get(k).contains(v)
+  // 1. Events that exist in old but not in new should be removed
+  val removedEvents = oldNode.events.keySet -- newNode.events.keySet
+
+  logger.debug(
+    "Identified removed events",
+    category = "Reconciler",
+    opId = 1,
+    Map("removedEvents" -> removedEvents.toString),
+  )
+
+  // 2. New events that didn't exist before should be added
+  val addedEvents = newNode.events.keySet -- oldNode.events.keySet
+
+  logger.debug(
+    "Identified added events",
+    category = "Reconciler",
+    opId = 1,
+    Map("addedEvents" -> addedEvents.toString),
+  )
+
+  // 3. For events that exist in both, we need to update if they've changed
+  val commonEvents = oldNode.events.keySet.intersect(newNode.events.keySet)
+
+  // Track which common events need updating by comparing the function references
+  val eventsToUpdate = commonEvents.filter { eventName =>
+    val needsUpdate = oldNode.events(eventName) ne newNode.events(eventName)
+    logger.debug(
+      s"Checking event $eventName for updates",
+      category = "Reconciler",
+      opId = 1,
+      Map("needsUpdate" -> needsUpdate.toString),
+    )
+    needsUpdate
   }
+
+  val eventsToRemove = removedEvents ++ eventsToUpdate
+  val eventsToAdd = newNode.events.filter { case (name, _) =>
+    addedEvents.contains(name) || eventsToUpdate.contains(name)
+  }
+
+  logger.debug(
+    "Final event changes",
+    category = "Reconciler",
+    opId = 1,
+    Map(
+      "eventsToRemove" -> eventsToRemove.toString,
+      "eventsToAdd"    -> eventsToAdd.toString,
+    ),
+  )
 
   if (
     propsToRemove.nonEmpty || propsToAdd.nonEmpty ||
