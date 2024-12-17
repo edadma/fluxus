25440cd35e25eb80791cafd72a56f8a0b2604bb8
Edward A Maxedon <edadma@gmail.com>
2024-12-17 06:53:45 -0500

    async tests

--- src/test/scala/io/github/edadma/fluxus/testing/HookTest.scala
+++ src/test/scala/io/github/edadma/fluxus/testing/HookTest.scala
@@ -5,7 +5,7 @@ import io.github.edadma.fluxus.core.{ComponentInstance, createDOM, reconcile}
 
 import scala.scalajs.js
 
-class HookTest extends AnyDOMSpec {
+class HookTest extends AsyncDOMSpec {
   "useState hook" should "maintain state between renders" in {
     val container = getContainer
     case class TestProps()
@@ -166,125 +166,126 @@ class HookTest extends AnyDOMSpec {
     container.querySelector(".count").textContent shouldBe "Count: 0"
     click(container.querySelector("button"))
 
-    js.timers.setTimeout(0) {
+    eventually {
       container.querySelector(".count").textContent shouldBe "Count: 2"
     }
   }
 
-//  it should "maintain multiple independent states in one component" in withDebugLogging(
-//    "multiple hooks stability",
-//  ) {
-//    val container   = getContainer
-//    var renderCount = 0
-//
-//    case class MultiCounterProps()
-//
-//    def MultiCounterComponent(props: MultiCounterProps): FluxusNode = {
-//      renderCount += 1
-//      logger.debug(
-//        "Rendering MultiCounterComponent",
-//        category = "Test",
-//        Map(
-//          "renderCount" -> renderCount.toString,
-//          "instance"    -> ComponentInstance.current.map(_.id).getOrElse("none"),
-//        ),
-//      )
-//
-//      val (count1, setCount1) = useState(0)
-//      val (count2, setCount2) = useState(10)      // Different initial value
-//      val (text, setText)     = useState("hello") // Different type
-//
-//      logger.debug(
-//        "Component state values",
-//        category = "Test",
-//        Map(
-//          "count1" -> count1.toString,
-//          "count2" -> count2.toString,
-//          "text"   -> text,
-//        ),
-//      )
-//
-//      div(
-//        cls := "multi-counter",
-//        div(cls := "count1", s"Count1: $count1"),
-//        div(cls := "count2", s"Count2: $count2"),
-//        div(cls := "text", s"Text: $text"),
-//        button(
-//          cls := "inc1",
-//          onClick := (() => {
-//            logger.debug(
-//              "Incrementing count1",
-//              category = "Test",
-//              Map("currentValue" -> count1.toString),
-//            )
-//            setCount1(count1 + 1)
-//          }),
-//          "Increment First",
-//        ),
-//        button(
-//          cls := "inc2",
-//          onClick := (() => {
-//            logger.debug(
-//              "Incrementing count2",
-//              category = "Test",
-//              Map("currentValue" -> count2.toString),
-//            )
-//            setCount2(count2 + 1)
-//          }),
-//          "Increment Second",
-//        ),
-//        button(
-//          cls := "setText",
-//          onClick := (() => {
-//            logger.debug(
-//              "Updating text",
-//              category = "Test",
-//              Map("currentText" -> text),
-//            )
-//            setText(text + "!")
-//          }),
-//          "Add Exclamation",
-//        ),
-//      )
-//    }
-//
-//    // Initial render
-//    val node = MultiCounterComponent <> MultiCounterProps()
-//    createDOM(node, container)
-//
-//    // Initial state
-//    renderCount shouldBe 1
-//    container.querySelector(".count1").textContent shouldBe "Count1: 0"
-//    container.querySelector(".count2").textContent shouldBe "Count2: 10"
-//    container.querySelector(".text").textContent shouldBe "Text: hello"
-//
-//    // Click first counter
-//    click(container.querySelector(".inc1"))
-//    js.timers.setTimeout(0) {
-//      renderCount shouldBe 2
-//      container.querySelector(".count1").textContent shouldBe "Count1: 1"
-//      container.querySelector(".count2").textContent shouldBe "Count2: 10" // Unchanged
-//      container.querySelector(".text").textContent shouldBe "Text: hello"  // Unchanged
-//    }
-//
-//    // Click second counter
-//    click(container.querySelector(".inc2"))
-//    js.timers.setTimeout(0) {
-//      renderCount shouldBe 3
-//      container.querySelector(".count1").textContent shouldBe "Count1: 1" // Unchanged
-//      container.querySelector(".count2").textContent shouldBe "Count2: 11"
-//      container.querySelector(".text").textContent shouldBe "Text: hello" // Unchanged
-//    }
-//
-//    // Update text
-//    click(container.querySelector(".setText"))
-//    js.timers.setTimeout(0) {
-//      renderCount shouldBe 4
-//      container.querySelector(".count1").textContent shouldBe "Count1: 1"  // Unchanged
-//      container.querySelector(".count2").textContent shouldBe "Count2: 11" // Unchanged
-//      container.querySelector(".text").textContent shouldBe "Text: hello!"
-//    }
-//  }
+  it should "maintain multiple independent states in one component" in withDebugLogging(
+    "multiple hooks stability",
+  ) {
+    val container   = getContainer
+    var renderCount = 0
+
+    case class MultiCounterProps()
+
+    def MultiCounterComponent(props: MultiCounterProps): FluxusNode = {
+      renderCount += 1
+      logger.debug(
+        "Rendering MultiCounterComponent",
+        category = "Test",
+        Map(
+          "renderCount" -> renderCount.toString,
+          "instance"    -> ComponentInstance.current.map(_.id).getOrElse("none"),
+        ),
+      )
+
+      val (count1, setCount1) = useState(0)
+      val (count2, setCount2) = useState(10)      // Different initial value
+      val (text, setText)     = useState("hello") // Different type
+
+      logger.debug(
+        "Component state values",
+        category = "Test",
+        Map(
+          "count1" -> count1.toString,
+          "count2" -> count2.toString,
+          "text"   -> text,
+        ),
+      )
+
+      div(
+        cls := "multi-counter",
+        div(cls := "count1", s"Count1: $count1"),
+        div(cls := "count2", s"Count2: $count2"),
+        div(cls := "text", s"Text: $text"),
+        button(
+          cls := "inc1",
+          onClick := (() => {
+            logger.debug(
+              "Incrementing count1",
+              category = "Test",
+              Map("currentValue" -> count1.toString),
+            )
+            setCount1(count1 + 1)
+          }),
+          "Increment First",
+        ),
+        button(
+          cls := "inc2",
+          onClick := (() => {
+            logger.debug(
+              "Incrementing count2",
+              category = "Test",
+              Map("currentValue" -> count2.toString),
+            )
+            setCount2(count2 + 1)
+          }),
+          "Increment Second",
+        ),
+        button(
+          cls := "setText",
+          onClick := (() => {
+            logger.debug(
+              "Updating text",
+              category = "Test",
+              Map("currentText" -> text),
+            )
+            setText(text + "!")
+          }),
+          "Add Exclamation",
+        ),
+      )
+    }
+
+    // Initial render
+    val node = MultiCounterComponent <> MultiCounterProps()
+    createDOM(node, container)
+
+    // Initial state
+    renderCount shouldBe 1
+    container.querySelector(".count1").textContent shouldBe "Count1: 0"
+    container.querySelector(".count2").textContent shouldBe "Count2: 10"
+    container.querySelector(".text").textContent shouldBe "Text: hello"
+
+    // Click first counter
+    click(container.querySelector(".inc1"))
+    eventually {
+      renderCount shouldBe 2
+      container.querySelector(".count1").textContent shouldBe "Count1: 1"
+      container.querySelector(".count2").textContent shouldBe "Count2: 10" // Unchanged
+      container.querySelector(".text").textContent shouldBe "Text: hello"  // Unchanged
+    }
+
+    // Click second counter
+    click(container.querySelector(".inc2"))
+    eventually {
+      renderCount shouldBe 3
+      container.querySelector(".count1").textContent shouldBe "Count1: 1" // Unchanged
+      container.querySelector(".count2").textContent shouldBe "Count2: 11"
+      container.querySelector(".text").textContent shouldBe "Text: hello" // Unchanged
+    }
+
+    // Update text
+    click(container.querySelector(".setText"))
+
+    eventually {
+      renderCount shouldBe 4
+      container.querySelector(".count1").textContent shouldBe "Count1: 1"  // Unchanged
+      container.querySelector(".count2").textContent shouldBe "Count2: 11" // Unchanged
+      container.querySelector(".text").textContent shouldBe "Text: hello!"
+    }
+  }
 
   it should "handle click events and state updates correctly" in /*withDebugLogging(
     "handle click events and state updates correctly",
@@ -337,7 +338,7 @@ class HookTest extends AnyDOMSpec {
     click(container.querySelector("button"))
 
     // Give React-style batching a chance to complete
-    js.timers.setTimeout(0) {
+    eventually {
       renderCount shouldBe 2
       container.querySelector(".count").textContent shouldBe "Count: 1"
     }
