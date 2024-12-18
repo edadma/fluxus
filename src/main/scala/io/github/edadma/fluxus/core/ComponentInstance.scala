4ed6e9897cccd1d30926ec305e87ef96d19475f0
Edward A Maxedon <edadma@gmail.com>
2024-12-18 17:01:23 -0500

    async tests

--- src/main/scala/io/github/edadma/fluxus/core/ComponentInstance.scala
+++ src/main/scala/io/github/edadma/fluxus/core/ComponentInstance.scala
@@ -27,34 +27,59 @@ case class ComponentInstance(
     var rendered: Option[FluxusNode] = None,
     var node: ComponentNode,
 ):
-  def rerender(): Unit =
+  def rerender(): Unit = {
     logger.debug(
       "Re-rendering component instance",
       category = "ComponentInstance",
-      Map("instanceId" -> id),
+      Map(
+        "instanceId"  -> id,
+        "hasRendered" -> rendered.isDefined,
+        "hasDomNode"  -> rendered.flatMap(_.domNode).isDefined,
+      ),
     )
 
     hookIndex = 0 // Reset hook index for new render
 
-    val parent = rendered.get.domNode.get.parentNode
+    rendered match {
+      case Some(oldNode) =>
+        oldNode.domNode match {
+          case Some(domNode) =>
+            val parent = domNode.parentNode
 
-    // Get new tree from component
-    val newNode = ComponentInstance.withInstance(this) {
-      node.component(node.props)
+            // Get new tree from component
+            val newNode = ComponentInstance.withInstance(this) {
+              node.component(node.props)
+            }
+
+            logger.debug(
+              "Reconciling component",
+              category = "ComponentInstance",
+              Map(
+                "oldNode" -> oldNode.toString,
+                "newNode" -> newNode.toString,
+              ),
+            )
+
+            // Use existing reconciliation
+            reconcile(rendered, Some(newNode), parent.asInstanceOf[org.scalajs.dom.Element])
+
+            rendered = Some(newNode)
+
+          case None =>
+            logger.error(
+              "Component instance has rendered node but no DOM node",
+              category = "ComponentInstance",
+              Map("instanceId" -> id),
+            )
+        }
+
+      case None =>
+        logger.error(
+          "Attempted to rerender component with no initial render",
+          category = "ComponentInstance",
+          Map("instanceId" -> id),
+        )
     }
-
-    logger.debug(
-      "Reconciling component",
-      category = "ComponentInstance",
-      Map(
-        "oldNode" -> rendered.toString,
-        "newNode" -> newNode.toString,
-      ),
-    )
-
-    // Use existing reconciliation
-    reconcile(rendered, Some(newNode), parent.asInstanceOf[org.scalajs.dom.Element])
-
-    rendered = Some(newNode)
+  }
 
   override def toString: String = s"ComponentInstance($id, type=$componentType)"
