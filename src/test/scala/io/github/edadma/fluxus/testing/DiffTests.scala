0fc2b036de76a05abf84cb13df915a7ed936b427
Edward A Maxedon <edadma@gmail.com>
2024-12-20 21:39:40 -0500

    reconciler tests

--- src/test/scala/io/github/edadma/fluxus/testing/DiffTests.scala
+++ src/test/scala/io/github/edadma/fluxus/testing/DiffTests.scala
@@ -1,37 +1,13 @@
 package io.github.edadma.fluxus.testing
 
 import io.github.edadma.fluxus.*
-import io.github.edadma.fluxus.core.DOMOperation.{InsertNode, Replace, UpdateText}
-import io.github.edadma.fluxus.core.{ComponentInstance, createDOM, diff, reconcile}
+import io.github.edadma.fluxus.core.{ComponentInstance, createDOM, diff, reconcile, InsertNode, Replace, UpdateText}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import pprint.pprintln
 
 class DiffTests extends AnyFlatSpec with Matchers {
-  "diff" should "handle optional content insertion correctly" in {
-    // Initial tree - no optional content
-    val oldTree = div(
-      button(onClick := (() => ()), "Toggle"),
-      div("last"),
-    )
-
-    // New tree - with optional content inserted
-    val newTree = div(
-      button(onClick := (() => ()), "Toggle"),
-      div("toggled"),
-      div("last"),
-    )
-
-    val ops = diff(Some(oldTree), Some(newTree))
-
-    pprintln(ops)
-    // Just insert the middle div, preserve the last div
-    ops should contain theSameElementsInOrderAs Seq(
-      InsertNode(div("toggled"), Some(1)),
-    )
-  }
-
   it should "handle simple text changes" in {
     val oldTree = TextNode("hello", None, None)
     val newTree = TextNode("world", None, None)
--- src/test/scala/io/github/edadma/fluxus/testing/RenderingTests.scala
+++ src/test/scala/io/github/edadma/fluxus/testing/RenderingTests.scala
@@ -1,43 +1,43 @@
-//package io.github.edadma.fluxus.testing
-//
-//import io.github.edadma.fluxus.*
-//import io.github.edadma.fluxus.core.{ComponentInstance, createDOM, reconcile}
-//
-//import scala.concurrent.Future
-//
-//class RenderingTests extends AsyncDOMSpec {
-//  "Optional content" should "appear in correct order regardless of empty div" in withDebugLogging(
-//    "appear in correct order regardless of empty div",
-//  ) {
-//    val container = getContainer
-//
-//    val TestComponent = () => {
-//      val (showText, setShowText) = useState(false)
-//
-//      div(
-//        button(
-//          onClick := (() => setShowText(!_)),
-//          "Toggle",
-//        ),
-//        Option.when(showText)(
-//          div("toggled"),
-//        ),
-//        div("last"),
-//      )
-//    }
-//
-//    createDOM(TestComponent <> (), container)
-//
-//    val buttonElem = container.querySelector("button")
-//
-//    eventually {
-//      container.innerHTML shouldBe "<div><button>Toggle</button><div>last</div></div>"
-//    }.map { _ =>
-//      click(buttonElem)
-//    }.flatMap { _ =>
-//      eventually {
-//        container.innerHTML shouldBe "<div><button>Toggle</button><div>toggled</div><div>last</div></div>"
-//      }
-//    }
-//  }
-//}
+package io.github.edadma.fluxus.testing
+
+import io.github.edadma.fluxus.*
+import io.github.edadma.fluxus.core.{ComponentInstance, createDOM, reconcile}
+
+import scala.concurrent.Future
+
+class RenderingTests extends AsyncDOMSpec {
+  "Optional content" should "appear in correct order regardless of empty div" in withDebugLogging(
+    "appear in correct order regardless of empty div",
+  ) {
+    val container = getContainer
+
+    val TestComponent = () => {
+      val (showText, setShowText) = useState(false)
+
+      div(
+        button(
+          onClick := (() => setShowText(!_)),
+          "Toggle",
+        ),
+        Option.when(showText)(
+          div("toggled"),
+        ),
+        div("last"),
+      )
+    }
+
+    createDOM(TestComponent <> (), container)
+
+    val buttonElem = container.querySelector("button")
+
+    eventually {
+      container.innerHTML shouldBe "<div><button>Toggle</button><div>last</div></div>"
+    }.map { _ =>
+      click(buttonElem)
+    }.flatMap { _ =>
+      eventually {
+        container.innerHTML shouldBe "<div><button>Toggle</button><div>toggled</div><div>last</div></div>"
+      }
+    }
+  }
+}
