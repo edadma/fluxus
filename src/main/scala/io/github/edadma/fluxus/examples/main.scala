ddef6d8f8322ca3df3d76a3e5d704bada3198a1e
Edward A Maxedon <edadma@gmail.com>
2024-12-22 12:22:25 -0500

    TimerApp

--- src/main/scala/io/github/edadma/fluxus/examples/main.scala
+++ src/main/scala/io/github/edadma/fluxus/examples/main.scala
@@ -2,4 +2,4 @@ package io.github.edadma.fluxus.examples
 
 import io.github.edadma.fluxus.render
 
-@main def run(): Unit = render(ButtonApp.App, "app")
+@main def run(): Unit = render(TimerApp.App, "app")
