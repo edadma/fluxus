0d9ce1979543df0ae9596bbf70ce38293d6d6a27
Edward A Maxedon <edadma@gmail.com>
2024-12-23 20:25:06 -0500

    MockServerTestApp

--- src/main/scala/io/github/edadma/fluxus/examples/main.scala
+++ src/main/scala/io/github/edadma/fluxus/examples/main.scala
@@ -2,4 +2,4 @@ package io.github.edadma.fluxus.examples
 
 import io.github.edadma.fluxus.render
 
-@main def run(): Unit = render(JSONPlaceholderApp.App, "app")
+@main def run(): Unit = render(MockServerTestApp.App, "app")
