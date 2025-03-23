cde4b7e9ffc5fdbb17efdd11e7e79b828e8173d8
Edward A Maxedon Sr <edadma@gmail.com>
2025-03-22 20:58:42 -0400

    fix useRef

--- examples/src/main/scala/io/github/edadma/fluxus/examples/main.scala
+++ examples/src/main/scala/io/github/edadma/fluxus/examples/main.scala
@@ -4,5 +4,5 @@ import io.github.edadma.fluxus.{logger, render}
 import io.github.edadma.logger.LogLevel
 
 @main def run(): Unit =
-  logger.setLogLevel(LogLevel.DEBUG)
+//  logger.setLogLevel(LogLevel.DEBUG)
   render(RefExampleApp.App, "app")
