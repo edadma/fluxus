48699fcf15b96be90f18852f90b574ed57185aac
Edward A Maxedon <edadma@gmail.com>
2025-04-06 11:48:37 -0400

    add QueryState library code

--- examples/src/main/scala/io/github/edadma/fluxus/examples/main.scala
+++ examples/src/main/scala/io/github/edadma/fluxus/examples/main.scala
@@ -1,8 +1,8 @@
-package io.github.edadma.fluxus.examples
-
-import io.github.edadma.fluxus.{logger, render}
-import io.github.edadma.logger.LogLevel
-
-@main def run(): Unit =
-//  logger.setLogLevel(LogLevel.DEBUG)
-  render(ToggleApp, "app")
+//package io.github.edadma.fluxus.examples
+//
+//import io.github.edadma.fluxus.{logger, render}
+//import io.github.edadma.logger.LogLevel
+//
+//@main def run(): Unit =
+////  logger.setLogLevel(LogLevel.DEBUG)
+//  render(ToggleApp, "app")
